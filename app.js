37052bdbbfb396806660cda930133449f262f9b8
Jarrett Croll <jarrett.croll@gmail.com>
2014-08-17 15:11:54 -0400

    first controller

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
-var app = angular.module('store', []);
+(function(){
+    var app = angular.module('store', []);
+    app.controller('StoreController', function(){
+
+    });
+
+    var gem  = {
+        name: 'Dodecahedron',
+        price: 2.95,
+        description: '...'
+    };
+})();
