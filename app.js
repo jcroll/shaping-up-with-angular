727057ca8f337f2b7733273fdccad44968f8a610
Jarrett Croll <jarrett.croll@gmail.com>
2014-08-17 15:50:10 -0400

    ng-repeat

--- app.js
+++ app.js
@@ -1,14 +1,23 @@
 (function(){
     var app = angular.module('store', []);
     app.controller('StoreController', function(){
-        this.product = gem;
+        this.products = gems;
     });
 
-    var gem  = {
+    var gems = [
+    {
         name: 'Dodecahedron',
         price: 2.95,
         description: '...',
         canPurchase: true,
         soldOut: false
-    };
+    },
+    {
+        name: 'Pentagonal Gem',
+        price: 5.95,
+        description: '...',
+        canPurchase: false,
+        soldOut: false
+    }
+    ];
 })();
